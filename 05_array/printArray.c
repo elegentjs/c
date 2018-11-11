49479405b0a52e170268c0d7a7111fd9dcb6997b
liupj <MooN_2012@qq.com>
2018-11-11 23:08:44 +0800

    update

---
+++ 05_array/printArray.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * 依次输入整型数组元素，并打印出来
+ * 
+ */
+int main(int argc, char const *argv[])
+{
+    int array[5];
+
+    puts("请输入5个整型数组元素：");
+    for (int index = 0; index < 5; index ++) {
+        scanf("%d\n", &array[index]);
+    }
+
+    for (int i = 0; i < 5; i ++) {
+        printf("array[%d] : %d \n", i, array[i]);
+    }
+
+    return 0;
+}
