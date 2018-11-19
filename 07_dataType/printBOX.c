d59e0d4285811413bd72b93f3ffbdffb97874f59
liupj <MooN_2012@qq.com>
2018-11-19 10:19:13 +0800

    update

---
+++ 07_dataType/printBOX.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+/**
+ * 输入一个正整数，分别打印它的二进制，八进制，十六进制
+ * 
+ */
+
+
+int countBits(unsigned);
+
+void printNumber(unsigned);
+
+int main(int argc, char const *argv[])
+{
+    puts("Please input a Integer number: ");
+
+    int number;
+
+    scanf("%d", &number);
+    printNumber(number);
+
+    return 0;
+}
+
+int countBits(unsigned number) {
+
+    int total = 0;
+
+    while (number) {
+        if (number & 1U) {
+            total ++;
+            number >>= 1;
+        }
+    }
+
+    return total;
+}
+
+
+void printNumber(unsigned number) {
+    int length = countBits(~0U);
+
+    printf("binary: ");
+    for (int index = length; index >= 0; index --) {
+      putchar(number >> index & 1U ? '1' : '0');  
+    }
+
+    puts("");
+
+    printf("oct: %o\n", number);
+    printf("hex: %x\n", number);
+}
