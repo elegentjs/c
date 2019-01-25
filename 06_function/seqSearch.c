b8d315a316df6bbe0757c19a22bcacbe05e128d6
liupj <liupj@nj-strongit.com.cn>
2019-01-25 11:09:00 +0800

    update

---
+++ 06_function/seqSearch.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#define NUMBER 10
+#define ERROR -1
+
+/**
+ * 顺序查找法
+ * 从数组的头开始依次搜索，直到找到目标元素为止
+ */
+int search(const int array[], int target);
+void display(int[]);
+
+int main(int argc, char const *argv[])
+{
+    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    puts("数组如下：");
+    display(array);
+
+    puts("请输入要查找的元素：");
+    int target;
+    scanf("%d", &target);
+
+    int index = search(array, target);
+
+    printf("要查找的元素%d在数组中的下标是：%d \n", target, index);
+
+    return 0;
+}
+
+/**
+ * 顺序查找指定元素，找到则返回对应数组下标，找不到则返回 -1
+ * 
+ */
+int search(const int array[], int target) {
+    int result = ERROR;
+    for (int index = 0; index < NUMBER; index ++) {
+        if (array[index] == target) {
+            result = index;
+            break;
+        }
+    }
+
+    return result;
+}
+
+
+void display(int array[]) {
+    for (int index = 0; index < NUMBER; index ++) {
+        printf("array[%d]:%2d   ", index, array[index]);
+    }
+
+    putchar('\n');
+}
