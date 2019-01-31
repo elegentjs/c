03aba288c5e0d847e9adaed067108a4023570db6
liupj <MooN_2012@qq.com>
2019-01-31 12:02:15 +0800

    update

---
+++ 11_stringAndPointer/strcopy.c
@@ -0,0 +1,16 @@
+#include "../header.h"
+
+char *strcopy(char *d, const char *s) {
+    while(*d++ = *s++);
+    return d;
+}
+
+int main(int argc, char const *argv[])
+{
+    char d[100];
+    strcopy(d, "你好！C Program Language");
+
+    printf("result : %s \n", d);
+
+    return 0;
+}
