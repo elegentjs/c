cc9e41c7024cad64695812ff5aa35daf2875435f
liupj <MooN_2012@qq.com>
2019-01-31 09:10:09 +0800

    update

---
+++ 10_pointer/array.c
@@ -0,0 +1,18 @@
+#include "../header.h"
+
+/**
+ * 数组与指针
+ * 
+ */
+int main(int argc, char const *argv[])
+{
+    int array[] = {10, 20, 30, 40, 50};
+
+    int *p = array;
+
+    for (int index = 0; index < 5; index ++)  {
+        printf("array[%d] = %d, p[%d] = %d, *(p + %d) = %d \n", index, array[index], index, p[index], index, *(p +  index));
+    }
+
+    return 0;
+}
