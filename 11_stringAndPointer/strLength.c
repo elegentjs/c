3a0294d55feb6d6de2cc1f7c98e76453041fe72e
liupj <MooN_2012@qq.com>
2019-01-31 11:29:29 +0800

    update

--- 11_stringAndPointer/strLength.c
+++ 11_stringAndPointer/strLength.c
@@ -1,10 +1,8 @@
 #include "../header.h"
 
-
 int main(int argc, char const *argv[])
 {
     char *s = "你好，C program language.";
-
     int length = strLength(s);
 
     printf("%s 长度： %d \n", s, length);
