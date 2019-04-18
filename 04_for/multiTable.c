e29f1095ced791f63c2fea5ae7166eb9ec278a84
liupj <MooN_2012@qq.com>
2019-04-18 11:27:40 +0800

    update

--- 04_for/multiTable.c
+++ 04_for/multiTable.c
@@ -3,14 +3,24 @@
 /**
  *  标准九九乘法表
  */
-int main(int argc, char const *argv[])
-{
+void printMultiTable() {
+
     for (int i = 1; i <= 9; i ++) {
         for (int j = 1; j <= i; j ++) {
-            printf("%d x %d = %2d  ", j, i, j * i);
+            printf("%d X %d = %2d ", j, i, i * j);    
         }
+
         putchar('\n');
     }
+}
+
+int main(int argc, char const *argv[])
+{
+    printMultiTable();
     
     return 0;
 }
+
+
+
+
