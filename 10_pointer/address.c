3cf1127ec5d8a647641de85d3fce1084b0b1e6a1
liupj <MooN_2012@qq.com>
2019-04-19 09:36:59 +0800

    update

--- 10_pointer/address.c
+++ 10_pointer/address.c
@@ -9,9 +9,9 @@ int main(int argc, char const *argv[])
     printf("x 的地址：    %p \n", &x);
     printf("y 的地址：    %p \n", &y);
     printf("z 的地址：    %p \n", &z);
-    printf("z[0] 的地址： %p \n", &z[0]);
-    printf("z[1] 的地址： %p \n", &z[1]);
-    printf("z[2] 的地址： %p \n", &z[2]);
+    printf("z[0]  的地址： %p \n", &z[0]);
+    printf("z[1]  的地址： %p \n", &z[1]);
+    printf("z[2]  的地址： %p \n", &z[2]);
     printf("z + 1 的地址： %p \n", &z + 1);
 
     return 0;
