f436cb9732efb97bd43ef6628eb66541bc10f065
liupj <MooN_2012@qq.com>
2019-04-19 12:00:18 +0800

    update

--- 14_advanced_memory/stackOverFlow.c
+++ 14_advanced_memory/stackOverFlow.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
+/**
+ *  演示数组访问越界
+ */
+
+void hello() {
+    fprintf(stderr, "hello!\n");
+}
+
+void func() {
+    void *buf[10];
+    static int i;
+
+    for (i = 0; i < 100; i ++) { // over flow
+        buf[i] = hello;
+    }
+
+}
+
 int main(int argc, char const *argv[])
 {
-    
+    int buf[1000];
+
+    func();
+
     return 0;
 }
